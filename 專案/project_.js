6c7cf359e272fd2136bcabe880ffa2679db35c79
Michelle-Wu03 <73873509+Michelle-Wu03@users.noreply.github.com>
2021-01-12 03:28:09 +0800

    Add files via upload

--- 專案/project_.js
+++ 專案/project_.js
@@ -1,20 +1,37 @@
 var array=[];
 var compound=["AgCl","PbCl2","Hg2Cl2","CuCl","AgBr","PbBr2","Hg2Br2","CuBr","AgI","PbI2","Hg2I2","CuI","CaSO4","SrSO4","BaSO4","PbSO4","CuS","Ag2S","NiS","ZnS",
 "AlOH3","FeOH2","ZnOH2","CuOH2","FeOH3","BaCrO4","PbCrO4","Ag2CrO4","MgCO3","CaCO3","SrCO3","BaCO3","NiCO3","CuCO3"];
-var colors=["white","white","white","white","yellow","yellow","yellow","yellow","yellow","yellow","yellow","yellow","white","white","white","white","black","black",
+var colors=["white","white","white","white","yellow","yellow","yellow","yellow","yellow","yellow","yellow, red","yellow","white","white","white","white","black","black",
 "black","white","white","white","white","blue","brown","yellow","yellow","firebrick","white","white","white","white","green","green"];
-var url=["https://zh.wikipedia.org/wiki/%E6%B0%AF%E5%8C%96%E9%93%B6","https://zh.wikipedia.org/wiki/%E6%B0%AF%E5%8C%96%E9%89%9B","https://zh.wikipedia.org/wiki/%E6%B0%AF%E5%8C%96%E4%BA%9A%E6%B1%9E",
-"https://zh.wikipedia.org/wiki/%E6%B0%AF%E5%8C%96%E4%BA%9A%E9%93%9C","https://zh.wikipedia.org/wiki/%E6%BA%B4%E5%8C%96%E9%8A%80","https://zh.wikipedia.org/zh-tw/%E6%BA%B4%E5%8C%96%E9%93%85",
-"https://en.wikipedia.org/wiki/Mercury(I)_bromide","https://zh.wikipedia.org/wiki/%E6%BA%B4%E5%8C%96%E4%BA%9A%E9%93%9C","https://zh.wikipedia.org/wiki/%E7%A2%98%E5%8C%96%E9%93%B6",
-"https://zh.wikipedia.org/wiki/%E7%A2%98%E5%8C%96%E9%89%9B","https://zh.wikipedia.org/wiki/%E7%A2%98%E5%8C%96%E6%B1%9E","https://zh.wikipedia.org/wiki/%E7%A2%98%E5%8C%96%E4%BA%9A%E9%93%9C",
-"https://zh.wikipedia.org/wiki/%E7%A1%AB%E9%85%B8%E9%92%99","https://zh.wikipedia.org/wiki/%E7%A1%AB%E9%85%B8%E9%8D%B6","https://zh.wikipedia.org/wiki/%E7%A1%AB%E9%85%B8%E9%92%A1",
-"https://zh.wikipedia.org/wiki/%E7%A1%AB%E9%85%B8%E9%93%85","https://zh.wikipedia.org/wiki/%E7%A1%AB%E5%8C%96%E9%8A%85","https://zh.wikipedia.org/wiki/%E7%A1%AB%E5%8C%96%E9%93%B6",
-"https://zh.wikipedia.org/wiki/%E7%A1%AB%E5%8C%96%E9%95%8D","https://zh.wikipedia.org/wiki/%E7%A1%AB%E5%8C%96%E9%94%8C","https://zh.wikipedia.org/wiki/%E6%B0%A2%E6%B0%A7%E5%8C%96%E9%93%9D",
-"https://zh.wikipedia.org/wiki/%E6%B0%A2%E6%B0%A7%E5%8C%96%E4%BA%9A%E9%93%81","https://zh.wikipedia.org/wiki/%E6%B0%AB%E6%B0%A7%E5%8C%96%E9%8B%85","https://zh.wikipedia.org/wiki/%E6%B0%A2%E6%B0%A7%E5%8C%96%E9%93%9C",
-"https://zh.wikipedia.org/wiki/%E6%B0%A2%E6%B0%A7%E5%8C%96%E9%93%81","https://zh.wikipedia.org/wiki/%E9%93%AC%E9%85%B8%E9%92%A1","https://zh.wikipedia.org/wiki/%E9%89%BB%E9%85%B8%E9%89%9B",
-"https://zh.wikipedia.org/wiki/%E9%93%AC%E9%85%B8%E9%93%B6","https://zh.wikipedia.org/wiki/%E7%A2%B3%E9%85%B8%E9%8E%82","https://zh.wikipedia.org/wiki/%E7%A2%B3%E9%85%B8%E9%88%A3",
-"https://zh.wikipedia.org/wiki/%E7%A2%B3%E9%85%B8%E9%94%B6","https://zh.wikipedia.org/wiki/%E7%A2%B3%E9%85%B8%E9%92%A1","https://zh.wikipedia.org/wiki/%E7%A2%B3%E9%85%B8%E9%8E%B3",
-"https://zh.wikipedia.org/wiki/%E7%A2%B3%E9%85%B8%E9%93%9C"];
+var url=["氯化銀","氯化鉛","氯化亚汞",
+"氯化亚铜","溴化銀","溴化铅",
+"溴化亚汞","溴化亚铜","碘化银",
+"碘化鉛","碘化汞","碘化亚铜",
+"硫酸钙","硫酸鍶","硫酸钡",
+"硫酸铅","硫化銅","硫化银",
+"硫化镍","硫化锌","氢氧化铝",
+"氢氧化亚铁","氫氧化鋅","氢氧化铜",
+"氢氧化铁","铬酸钡","鉻酸鉛",
+"铬酸银","碳酸鎂","碳酸鈣",
+"碳酸锶","碳酸钡","碳酸鎳",
+"碳酸铜"];
+var formu= ["2 Ag + 2 HCl → 2 AgCl + H2", "PbCO3 + 2 HCl(aq) → PbCl2(s) + CO2(g) + H2O <br> Pb(CH3COO)2(aq) + HCl(aq)→ PbCl2(s) + 2CH3COOH(aq)", "2 HgCl2 + SO2 + 2 H2O → Hg2Cl2↓ + 2 HCl + H2SO4<br> HgCl2 + Hg → Hg2Cl2 <br> 2HCl + Hg2(NO3)2 → Hg2Cl2↓ + 2HNO3",
+"HgCl2 + 2 Cu → 2 CuCl + Hg", "MBr + AgNO3 → AgBr↓ + MNO3 (M = H、鹼金屬)", "2 NaBr + Pb(NO3)2 → 2 NaNO3 + PbBr2↓"
+, "", "2 CuBr2 + H2O + SO32− → 2 CuBr + SO42− + 2 HBr<br>2 HBr + 2 Cu → 2 CuBr + H2", "", 
+"Pb(NO3)2 + 2 KI → PbI2 ↓ + 2 KNO3<br>Pb + 2 HI → PbI2 ↓ + H2 ↑", "HgCl2 + 2 KI → HgI2 + 2 KCl<br>Hg + I2 → HgI2","2 Cu2+ + 4 I− → 2 CuI + I2",
+"．通常硫酸鈣由天然礦物中提取<br>H2SO4 + Ca(OH)2 → CaSO4 + 2 H2O<br>H2SO4 + CaCO3 → CaSO4 + CO2 + H2O<br>H2SO4 + Ca → CaSO4 + H2", "H2SO4 + BaCl2 → 2 HCl + BaSO4↓",
+"Pb(NO3)2 + Na2SO4 → PbSO4↓ + 2NaNO3<br>PbO + H2SO4 → PbSO4↓ + H2O<br>Pb + H2SO4 → PbSO4↓ + H2↑<br>PbS + 4 H2O2 → PbSO4↓ + 4 H2O", "", "",
+"Ni2+ + H2S → NiS↓ + 2 H+", "Zn2+ + H2S → ZnS↓ + 2 H+<br>Zn + S → ZnS", "Zn2+ + H2S → ZnS↓ + 2 H+<br>Zn + S → ZnS", "Al3+ + 3NH3·H2O → Al(OH)3↓ + 3NH4+<br>[Al(OH)4]− + H+ → H2O + Al(OH)3",
+"4 Fe(OH)2 + O2 + 2 H2O → 4 Fe(OH)3", "", "2 Cu(s) + H2O(g) + CO2(g) + O2(g) → Cu(OH)2·CuCO3(s)",
+"Fe3+ + 3 OH− → Fe(OH)3<br>4Fe(OH)2 + O2 + 2H2O → 4Fe(OH)3", "","Pb2+ + CrO42− → PbCrO4",
+"K2CrO4 + 2 AgNO3 → Ag2CrO4 + 2 KNO3", "Mg(OH)2 + 2 CO2 → Mg(HCO3)2 得到可溶的碳酸氫鎂，接著將混合液過濾，<br>濾液在真空乾燥下得到含有碳酸鎂的水溶液 Mg2+ + 2 HCO3- → MgCO3 + CO2 + H2O","Na2CO3(aq) + CaCl2(aq) → CaCO3(s)↓ + 2NaCl(aq)",
+"Sr(NO3)2 (aq) + Na2CO3 (aq) → SrCO3 (s) + 2 NaNO3 (aq)", "鋇是重金屬，碳酸鋇能與胃液中的鹽酸反應，釋放出鋇離子，引起中毒反應。因此碳酸鋇是劇毒品!要小心!", "4 Ni2+ + Na2CO3 + 6 OH- + 4 H2O → Ni4CO3(OH)6(H2O)4 + 2 Na+",
+"2Cu(s) + H2O(g) + CO2 + O2 → Cu(OH)2 + CuCO3(s)"]
+var  obj = {}
+for (i = 0; i < compound.length; i++){
+    obj[compound[i]] = [colors[i], url[i], formu[i]];
+    //console.log(obj[compound[i]]);
+}
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -41,7 +58,7 @@ function start(){
 
 function openwin()
 {
-    console.log(array)
+    //console.log(array)
     var len1=array[0].length;
     var len2=array[1].length;
     if (array[0][len1-1] == "-"){
@@ -49,7 +66,7 @@ function openwin()
         array[0] = array[1];
         array[1] = t;
     }
-    console.log(array)
+    //console.log(array)
     var num1="";
     var num2="";
     len1=array[0].length;
@@ -77,21 +94,11 @@ function openwin()
         num2="";
     }
     var string=elem1+num2+elem2+num1;
-    console.log(string);
-    var color_mem="";
-    var url_mem="";
-    for(var i=0;i<compound.length;i++)
-    {
-        if(compound[i]==string)
-        {
-            color_mem=colors[i];
-            url_mem=url[i];
-        }
-    }
-    console.log(color_mem);
-    if(color_mem=="")
-    {
-        window.alert("查無資料");
+    //console.log(string);
+    //console.log(Object.keys(obj))
+    if (Object.keys(obj).indexOf(string) < 0) {
+            $().animation();
+            return;
     }
     else
     {
@@ -99,8 +106,11 @@ function openwin()
         OpenWindow.document.write("<TITLE>例子</TITLE>");
         OpenWindow.document.write("<BODY BGCOLOR=#ffffff>")
         OpenWindow.document.write("<h1>Hello!"+string+"</h1>");
-        OpenWindow.document.write("<h1>"+string+" color is "+color_mem+"</h1>");
-        OpenWindow.document.write("<a href='"+url_mem+"'><img src='compound/"+string+".jpg' width=300 height=300></a>");
+        OpenWindow.document.write("<h1>"+string+" color is "+obj[string][0]+"</h1>");
+        OpenWindow.document.write("<p><em>For more information, please click on the image ↓</em></p>");
+        OpenWindow.document.write("<a href='https://zh.wikipedia.org/wiki/"+obj[string][1]+"', target = '_blank'><img src='compound/"+string+".jpg' width=300 height=300></a>");
+        OpenWindow.document.write("<p>" + obj[string][2] +"</p>");
+        OpenWindow.document.write("<img src='magic.jpg' width=300 height=300 style = 'position: fixed; right: 0;bottom:0; height: 30%; width: 30%; z-index: -1;'>");
         OpenWindow.document.write("</BODY>");
         OpenWindow.document.write("</HTML>");
         OpenWindow.document.close()
